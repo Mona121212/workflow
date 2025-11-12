87456a2f078412aeef8eb7fbfa5b21495cd0390c
Mona <xiaomei.mona.he@gmail.com>
2025-11-12 12:06:33 -0700

    chore: remove invalidate codes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    //config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
   },
 };
